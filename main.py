e12c27b370ad84b62b1d38be56a40c579b550682
parassareen4 <kyakrnahaibro@gmail.com>
2025-03-14 12:30:21 -0400

    dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-from scrape import scrape_website
+from scrape import scrape_website, extract_body_content, clean_body_content, split_dom_content
 
 st.title("Wisemen")
 url = st.text_input("Enter the URL: ")
@@ -7,4 +7,11 @@ url = st.text_input("Enter the URL: ")
 if st.button("Scrape Site"):
     st.write("Scraping...")
     result = scrape_website(url)
-    print(result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+    
+    st.session_state.dom_content = cleaned_content
+    
+    with st.expander("View Content"):
+        st.text_area("DOM Content",cleaned_content,height=300)
+
