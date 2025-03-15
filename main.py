9e9d9e1e1363bbb9ab16e4b3be00c29ab56af2fe
parassareen4 <kyakrnahaibro@gmail.com>
2025-03-15 01:40:03 -0400

    dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 from scrape import scrape_website, extract_body_content, clean_body_content, split_dom_content
+from parse import parse_with_ollama
 
 st.title("Wisemen")
 url = st.text_input("Enter the URL: ")
@@ -15,3 +16,14 @@ if st.button("Scrape Site"):
     with st.expander("View Content"):
         st.text_area("DOM Content",cleaned_content,height=300)
 
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("describe what you want to parse?")
+    
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing...")
+            
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            results = parse_with_ollama(dom_chunks,parse_description)
+            
+            st.write(results)
